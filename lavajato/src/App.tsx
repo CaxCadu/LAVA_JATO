69a6a53fa711df6d1cf12ba68b01ec37c450dc93
Cadu <carlos.s.andrade2602@gmail.com>
2026-01-09 21:05:41 +0000

    commit 1

---
+++ lavajato/src/App.tsx
@@ -0,0 +1,45 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lava Jato</h1>
+
+      <div>
+        <div className='receitadiaria'>
+          <p>Total do dia $</p>
+
+          <p>Lavagens:</p>
+          <p>Estacionamento:</p>
+        </div>
+
+        <div className='receitamensal'>
+          <p>Receita mensal:</p>
+        </div>
+
+        <div>
+          <p>Serviços pendentes:</p>
+        </div>
+
+        <div>
+          <p>Lucro líquido:</p>
+
+          <p>Despesas:</p>
+          <p>Receita total:</p>
+        </div>
+
+        <div>
+          <p>Lavadores ativos:</p>
+        </div>
+
+        <div>
+          <p>Serviços por Lavador:</p>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
